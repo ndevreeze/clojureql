f81df637e919f7617aa787443fa8cbdb8696ceb3
ndevreeze <nico@ndv.nl>
2025-08-24 20:56:46 +0200

    Set v1.0.6, use recent postgres library

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clojureql "1.0.5"
+(defproject clojureql "1.0.6"
   :description      "Superior SQL integration for Clojure"
   :url              "http://clojureql.sabrecms.com"
   :license          "MIT"
@@ -9,7 +9,9 @@
                      [org.clojure/tools.nrepl     "0.2.12"]
                      [mysql/mysql-connector-java  "5.1.17"]
                      [org.xerial/sqlite-jdbc      "3.7.2"]
-                     [postgresql/postgresql       "8.4-702.jdbc4"]
+                     ;; [postgresql/postgresql       "8.4-702.jdbc4"]
+                     ;; 2025-08-24: recent lib, supporting support the SCRAM-SHA-256 authentication
+                     [org.postgresql/postgresql "42.7.7"]
                      [org.apache.derby/derby      "10.1.1.0"]]
   :repositories {"clojure-releases"  {:url "http://build.clojure.org/releases"}
                  "clojure-snapshots"  {:url "http://build.clojure.org/snapshots"}})
